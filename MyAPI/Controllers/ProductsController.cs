d5e6d68dd996a31c4b777598651d6a4dc140085d
Alexandr <alexandrbelen@gmail.com>
2021-06-30 20:26:04 +0300

    feat: add paginating and filtering for products

--- MyAPI/Controllers/ProductsController.cs
+++ MyAPI/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyAPI.Context;
+using MyAPI.Helpers;
 using MyAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -23,9 +24,23 @@ namespace MyAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllProducts()
+        public async Task<IActionResult> GetAllProducts([FromQuery]ProductQueryParametrs queryParametrs)
         {
-            return Ok(await _context.Products.ToArrayAsync());
+            IQueryable<Product> products = _context.Products;
+
+            if(queryParametrs.MinPrice!=null && queryParametrs.MaxPrice != null)
+            {
+                products = products.Where(p => p.Price >= queryParametrs.MinPrice && p.Price <= queryParametrs.MaxPrice);
+            }
+            if (!string.IsNullOrEmpty(queryParametrs.Sku))
+            {
+                products = products.Where(p => p.Sku == queryParametrs.Sku);
+            }
+            products = products
+                .Skip(queryParametrs.Size * (queryParametrs.Page - 1))
+                .Take(queryParametrs.Size);
+
+            return Ok(await products.ToArrayAsync());
         }
 
         [HttpGet, Route("/products/{id}")]
---
+++ MyAPI/Helpers/ProductQueryParametrs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPI.Helpers
+{
+    public class ProductQueryParametrs : QueryParametrs
+    {
+        public string Sku { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+    }
+}
