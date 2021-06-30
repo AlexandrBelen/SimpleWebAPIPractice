e0390e9862ff85e1fbfd602f699f149082ae1271
Alexandr <alexandrbelen@gmail.com>
2021-06-30 16:21:29 +0300

    feat: adding ProductsController

--- MyAPI/Context/DataContext.cs
+++ MyAPI/Context/DataContext.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MyAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyAPI.Context
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(a => a.Category)
+                .HasForeignKey(a => a.CategoryId);
+
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Products);
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.User);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Orders)
+                .WithOne(o => o.User)
+                .HasForeignKey(o => o.UserId);
+
+            modelBuilder.Seed();
+
+        }
     }
 }
---
+++ MyAPI/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyAPI.Context;
+using MyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext dataContext)
+        {
+            _context = dataContext;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            return Ok(await _context.Products.ToArrayAsync());
+        }
+
+        [HttpGet, Route("/products/{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        
+    }
+}
