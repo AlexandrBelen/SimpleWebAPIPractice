fd4f23f907ae8fe8c6445b8f872502b418085dca
Alexandr <alexandrbelen@gmail.com>
2021-07-01 16:04:46 +0300

    feat: add post, put and delete methods for product

--- MyAPI/Controllers/ProductsController.cs
+++ MyAPI/Controllers/ProductsController.cs
@@ -28,6 +28,8 @@ namespace MyAPI.Controllers
         {
             IQueryable<Product> products = _context.Products;
 
+            if(ModelState.IsValid)
+
             if(queryParametrs.MinPrice!=null && queryParametrs.MaxPrice != null)
             {
                 products = products.Where(p => p.Price >= queryParametrs.MinPrice && p.Price <= queryParametrs.MaxPrice);
@@ -74,6 +76,76 @@ namespace MyAPI.Controllers
             return Ok(product);
         }
 
-        
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute]int id, [FromBody] Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(product).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Products.Find(id) == null)
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<ActionResult<Product>> DeleteProducts([FromQuery]int[] ids)
+        {
+            var products = new List<Product>();
+            foreach(var id in ids)
+            {
+                var product = await _context.Products.FindAsync(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                products.Add(product);
+            }
+            
+
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+
+            return Ok(products);
+        }
+
     }
 }
